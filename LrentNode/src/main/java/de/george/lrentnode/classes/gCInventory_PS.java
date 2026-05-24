36d9cda104fc42e8cdb8f1ef63b0a6bd9076e309
Georgeto <George.to@gmx.de>
2026-05-24 13:58:08 +0200

    Cleanup gCInventory_PS read/write

--- LrentNode/src/main/java/de/george/lrentnode/classes/gCInventory_PS.java
+++ LrentNode/src/main/java/de/george/lrentnode/classes/gCInventory_PS.java
@@ -9,11 +9,17 @@ import org.slf4j.LoggerFactory;
 
 import de.george.g3utils.io.G3FileReader;
 import de.george.g3utils.io.G3FileWriter;
+import de.george.g3utils.util.Misc;
 import de.george.lrentnode.classes.desc.CD;
+import de.george.lrentnode.enums.G3Enums;
+import de.george.lrentnode.enums.G3Enums.gESlot;
 import de.george.lrentnode.util.ClassUtil;
 
 public class gCInventory_PS extends G3Class {
 	private static final Logger logger = LoggerFactory.getLogger(gCInventory_PS.class);
+
+	private static final int SLOT_COUNT = G3Enums.maxValue(gESlot.class) + 1;
+	private static final byte[] EMPTY_SLOT = Misc.asByte("010000");
 
 	public List<G3Class> stacks;
 	public List<G3Class> slots;
@@ -26,7 +32,6 @@ public class gCInventory_PS extends G3Class {
 
 	@Override
 	protected void readPostClassVersion(G3FileReader reader) {
-		// TODO: Improve
 		slots = new ArrayList<>();
 		errSlots = new HashMap<>();
 
@@ -37,6 +42,9 @@ public class gCInventory_PS extends G3Class {
 		// Inventory Slots
 		reader.skip(2);
 		int slotCount = reader.readInt();
+		if (slotCount != SLOT_COUNT)
+			reader.raiseError(logger, "Expected {} slots, got {} slots", SLOT_COUNT, slotCount);
+
 		for (int i = 0; i < slotCount; i++) {
 			if (reader.readSilent(2, 1).equals("01")) {
 				G3Class slot = ClassUtil.readSubClass(reader);
@@ -48,7 +56,7 @@ public class gCInventory_PS extends G3Class {
 
 				slots.add(slot);
 			} else {
-				reader.skip(3);
+				reader.expectOrRaise(EMPTY_SLOT, "Expected empty slot");
 			}
 		}
 		reader.skip(15);
@@ -56,14 +64,13 @@ public class gCInventory_PS extends G3Class {
 
 	@Override
 	protected void writePostClassVersion(G3FileWriter writer) {
-		// StringBuffer buffer = new StringBuffer();
-		// Stacks
-		writer.write("0100");
+		writer.writeUnsignedShort(1);
 		writer.writeList(stacks, ClassUtil::writeSubClass);
 
 		// Slots
-		writer.write("010013000000");
-		for (int i = 0; i < 19; i++) {
+		writer.writeUnsignedShort(1);
+		writer.writeInt(SLOT_COUNT);
+		for (int i = 0; i < SLOT_COUNT; i++) {
 			boolean found = false;
 			for (G3Class slot : slots) {
 				if (errSlots.containsKey(slot)) {
@@ -79,7 +86,7 @@ public class gCInventory_PS extends G3Class {
 				}
 			}
 			if (!found) {
-				writer.write("010000");
+				writer.write(EMPTY_SLOT);
 			}
 		}
 		writer.write("010000010000010000010000010000");
